17f46cbb7735763b30f648bb2bc79c8295485636
Christophe Gabard <cgabard@users.noreply.github.com>
2016-05-11 16:57:25 +0200

    Specific zds-extensions tests : comments, del, kbd, subscript et superscript (#88)
    + Add "~" and "^" as escaped char

--- markdown/extensions/comments.py
+++ markdown/extensions/comments.py
@@ -1,4 +1,4 @@
-# Comment extension by cgabard
+# Comment extension
 # inspired by  https://github.com/ryneeverett/python-markdown-comments
 
 import re
@@ -7,24 +7,24 @@ from markdown.extensions import Extension
 
 
 class CommentsExtension(Extension):
-    def __init__(self, config={}):
-        self.config = {"START_TAG": "<--COMMENTS",
-                       "END_TAG": "COMMENTS-->"}
-
-        for key, value in config.items():
-            self.config[key] = value
+    def __init__(self, *args, **kwargs):
+        self.config = {"start_tag": ["<--COMMENTS", ""],
+                       "end_tag":   ["COMMENTS-->", ""]}
+        Extension.__init__(self, *args, **kwargs)
 
     def extendMarkdown(self, md, md_globals):
         md.registerExtension(self)
-        md.preprocessors.add("comments", CommentsProcessor(md, self.config), ">fenced_code_block")
+        md.preprocessors.add("comments",
+                             CommentsProcessor(md, self.getConfig("start_tag"), self.getConfig("end_tag")),
+                             ">fenced_code_block")
 
 
 class CommentsProcessor(Preprocessor):
-    def __init__(self, md, config={}):
+    def __init__(self, md, start_tag, end_tag):
         Preprocessor.__init__(self, md)
 
-        StaEsc = re.escape(config["START_TAG"])
-        EndEsc = re.escape(config["END_TAG"])
+        StaEsc = re.escape(start_tag)
+        EndEsc = re.escape(end_tag)
 
         self.RE = re.compile(StaEsc + r'.*?' + EndEsc, re.MULTILINE | re.DOTALL)
 
@@ -37,3 +37,7 @@ class CommentsProcessor(Preprocessor):
             else:
                 break
         return text.split("\n")
+
+
+def makeExtension(*args, **kwargs):
+    return CommentsExtension(*args, **kwargs)
