8c62ff55bc016f5754097acb9e9787b3412ca4ee
Christophe Gabard <cgabard@users.noreply.github.com>
2016-05-11 10:13:24 +0200

    Specific zds-extensions tests : video (#86)
    * Specific zds-extensions tests : video
    
    Tests + fix for #72, include #70 and add ina support (see https://github.com/zestedesavoir/zds-site/pull/2927)
    
    * Small commit only to unlock travis
    
    * Update video.py

--- markdown/extensions/video.py
+++ markdown/extensions/video.py
@@ -7,194 +7,166 @@ import re
 
 
 class VideoExtension(markdown.Extension):
-    def __init__(self, js_support=False, configs={}):
+    def __init__(self, js_support=False, **kwargs):
+        markdown.Extension.__init__(self)
+
         self.config = {
             'dailymotion_width': ['480', 'Width for Dailymotion videos'],
             'dailymotion_height': ['270', 'Height for Dailymotion videos'],
-            'metacafe_width': ['440', 'Width for Metacafe videos'],
-            'metacafe_height': ['248', 'Height for Metacafe videos'],
-            'veoh_width': ['410', 'Width for Veoh videos'],
-            'veoh_height': ['341', 'Height for Veoh videos'],
             'vimeo_width': ['500', 'Width for Vimeo videos'],
             'vimeo_height': ['281', 'Height for Vimeo videos'],
             'yahoo_width': ['624', 'Width for Yahoo! videos'],
             'yahoo_height': ['351', 'Height for Yahoo! videos'],
             'youtube_width': ['560', 'Width for Youtube videos'],
             'youtube_height': ['315', 'Height for Youtube videos'],
+            'ina_width': ['620', 'Width for INA videos'],
+            'ina_height': ['349', 'Height for INA videos'],
             'jsfiddle': [False, ''],
             'jsfiddle_width': ['560', 'Width for jsfiddle'],
             'jsfiddle_height': ['560', 'Height for jsfiddle'],
         }
 
+        self.config['youtube_short_width'] = self.config['youtube_width']
+        self.config['youtube_short_height'] = self.config['youtube_height']
+
         # Override defaults with user settings
-        for key, value in configs:
+        for key, value in kwargs.items():
             self.setConfig(key, value)
+
         if js_support:
             self.setConfig("jsfiddle", True)
 
     def add_inline(self, md, name, klass, pat):
         RE = r'(^|\n)!\(' + pat + r'\)'
-        md.parser.blockprocessors.add("video-" + name, VideoBProcessor(md, name, klass, RE, self.config), "_begin")
+        md.parser.blockprocessors.add("video-" + name,
+                                      klass(md, RE,
+                                            self.config["{}_width".format(name)][0],
+                                            self.config["{}_height".format(name)][0]),
+                                      "_begin")
 
     def extendMarkdown(self, md, md_globals):
         self.add_inline(md, 'dailymotion', Dailymotion,
                         r'https?://www\.dailymotion\.com/video/(?P<dailymotionid>[a-z0-9]+)(_[\w\-]*)?')
-        self.add_inline(md, 'metacafe', Metacafe,
-                        r'http://www\.metacafe\.com/watch/(?P<metacafeid>\d+)/?(:?.+/?)')
-        self.add_inline(md, 'veoh', Veoh,
-                        r'http://www\.veoh\.com/\S*(#watch%3D|watch/)(?P<veohid>\w+)')
         self.add_inline(md, 'vimeo', Vimeo,
-                        r'http://(www.|)vimeo\.com/(?P<vimeoid>\d+)\S*')
+                        r'https?://(www.|)vimeo\.com/(?P<vimeoid>\d+)\S*')
         self.add_inline(md, 'yahoo', Yahoo,
-                        r'http://screen\.yahoo\.com/.+/?')
+                        r'https?://screen\.yahoo\.com/.+/?')
         self.add_inline(md, 'youtube', Youtube,
-                        r'https?://www\.youtube\.com/watch\?\S*v=(?P<youtubeid>\S[^&/]+)')
+                        r'https?://(www\.)?youtube\.com/watch\?\S*v=(?P<youtubeid>\S[^&/]+)'
+                        r'(?P<channel>&ab_channel=[\w%]+)?')
         self.add_inline(md, 'youtube_short', Youtube,
                         r'https?://youtu\.be/(?P<youtubeid>\S[^?&/]+)?')
+        self.add_inline(md, 'ina', Ina,
+                        r'https?://www\.ina\.fr/video/(?P<inaid>[A-Z0-9]+)/([\w\-]*)\.html')
         if self.config["jsfiddle"][0]:
             self.add_inline(md, 'jsfiddle', JsFiddle,
-                            r'https?://(www.|)jsfiddle\.net/(?P<jsfiddleid>[a-z0-9]+)/(?P<jsfiddlerev>[0-9]+)/?')
+                            r'https?://(www.|)jsfiddle\.net(/(?P<jsfiddleuser>\w+))?/'
+                            r'(?P<jsfiddleid>\w+)(/(?P<jsfiddlerev>[0-9]+)|)/?')
 
 
 class VideoBProcessor(BlockProcessor):
-    def __init__(self, md, name, klass, patt, config):
+    def __init__(self, md, patt, width, height):
+        BlockProcessor.__init__(self, md.parser)
         self.md = md
-        self.name = name
-        self.klass = klass(config)
+        self.width = width
+        self.height = height
         self.RE = re.compile(patt)
 
     def test(self, parent, block):
         return bool(self.RE.search(block))
 
     def run(self, parent, blocks):
+        m = self.RE.search(blocks[0])
+
+        el = self.handle_match(m)
+        if el is None:
+            return False
+
         block = blocks.pop(0)
-
-        m = self.RE.search(block)
-
         before = block[:m.start()]
         after = block[m.end():]
 
-        if before:
+        if before:  # pragma: no cover
+            # This should never occur because regex require that the expression is starting the block.
+            # Do not raise an exception because exception should never be generated.
             self.md.parser.parseBlocks(parent, [before])
 
-        el = self.klass.handleMatch(m)
         parent.append(el)
 
         if after:
             blocks.insert(0, after)
 
+    @staticmethod
+    def extract_url(m):  # pragma: no cover
+        # Should be overridden in sub-class
+        return ""
 
-class Dailymotion(object):
-    def __init__(self, config):
-        self.config = config
+    def handle_match(self, m):
+        url = self.extract_url(m)
+        if url is None:
+            return None
+        return self.render_iframe(url, self.width, self.height)
 
-    def handleMatch(self, m):
-        url = 'https://www.dailymotion.com/embed/video/%s' % m.group('dailymotionid')
-        width = self.config['dailymotion_width'][0]
-        height = self.config['dailymotion_height'][0]
-        return render_iframe(url, width, height)
-
-
-class Metacafe(object):
-    def __init__(self, config):
-        self.config = config
-
-    def handleMatch(self, m):
-        url = 'https://www.metacafe.com/embed/%s/' % m.group('metacafeid')
-        width = self.config['metacafe_width'][0]
-        height = self.config['metacafe_height'][0]
-        return render_iframe(url, width, height)
-
-
-class Veoh(object):
-    def __init__(self, config):
-        self.config = config
-
-    def handleMatch(self, m):
-        url = 'https://www.veoh.com/videodetails2.swf?permalinkId=%s' % m.group('veohid')
-        width = self.config['veoh_width'][0]
-        height = self.config['veoh_height'][0]
-        return flash_object(url, width, height)
+    @staticmethod
+    def render_iframe(url, width, height):
+        iframe = etree.Element('iframe')
+        iframe.set('width', width)
+        iframe.set('height', height)
+        iframe.set('src', url)
+        iframe.set('allowfullscreen', 'true')
+        iframe.set('frameborder', '0')
+        return iframe
 
 
-class Vimeo(object):
-    def __init__(self, config):
-        self.config = config
-
-    def handleMatch(self, m):
-        url = 'https://player.vimeo.com/video/%s' % m.group('vimeoid')
-        width = self.config['vimeo_width'][0]
-        height = self.config['vimeo_height'][0]
-        return render_iframe(url, width, height)
+class Dailymotion(VideoBProcessor):
+    @staticmethod
+    def extract_url(m):
+        return 'https://www.dailymotion.com/embed/video/%s' % m.group('dailymotionid')
 
 
-class Yahoo(object):
-    def __init__(self, config):
-        self.config = config
-
-    def handleMatch(self, m):
-        url = m.string + '?format=embed&player_autoplay=false'
-        width = self.config['yahoo_width'][0]
-        height = self.config['yahoo_height'][0]
-        return render_iframe(url, width, height)
+class Vimeo(VideoBProcessor):
+    @staticmethod
+    def extract_url(m):
+        return 'https://player.vimeo.com/video/%s' % m.group('vimeoid')
 
 
-class Youtube(object):
-    def __init__(self, config):
-        self.config = config
-
-    def handleMatch(self, m):
-        url = 'https://www.youtube.com/embed/%s' % m.group('youtubeid')
-        width = self.config['youtube_width'][0]
-        height = self.config['youtube_height'][0]
-        return render_iframe(url, width, height)
+class Yahoo(VideoBProcessor):
+    @staticmethod
+    def extract_url(m):
+        return m.string + '?format=embed&player_autoplay=false'
 
 
-class JsFiddle(object):
-    def __init__(self, config):
-        self.config = config
-
-    def handleMatch(self, m):
-        url = "https://jsfiddle.net/{}/{}/embedded/result,js,html,css/".format(m.group('jsfiddleid'),
-                                                                               m.group('jsfiddlerev'))
-
-        width = self.config['jsfiddle_width'][0]
-        height = self.config['jsfiddle_height'][0]
-        return render_iframe(url, width, height)
+class Youtube(VideoBProcessor):
+    @staticmethod
+    def extract_url(m):
+        return 'https://www.youtube.com/embed/%s' % m.group('youtubeid')
 
 
-def render_iframe(url, width, height):
-    iframe = etree.Element('iframe')
-    iframe.set('width', width)
-    iframe.set('height', height)
-    iframe.set('src', url)
-    iframe.set('allowfullscreen', 'true')
-    iframe.set('frameborder', '0')
-    return iframe
+class Ina(VideoBProcessor):
+    @staticmethod
+    def extract_url(m):
+        return 'http://player.ina.fr/player/embed/%s/1/1b0bd203fbcd702f9bc9b10ac3d0fc21/560/315/1/148db8' % m.group(
+            'inaid')
 
 
-def flash_object(url, width, height):
-    obj = etree.Element('object')
-    obj.set('type', 'application/x-shockwave-flash')
-    obj.set('width', width)
-    obj.set('height', height)
-    obj.set('data', url)
-    param = etree.Element('param')
-    param.set('name', 'movie')
-    param.set('value', url)
-    obj.append(param)
-    param = etree.Element('param')
-    param.set('name', 'allowFullScreen')
-    param.set('value', 'true')
-    obj.append(param)
-    return obj
+class JsFiddle(VideoBProcessor):
+    @staticmethod
+    def extract_url(m):
+        fields = (m.group('jsfiddleuser'), m.group('jsfiddleid'), m.group('jsfiddlerev'))
+        if fields[0] is not None and fields[2] is None:
+            # Only two part, revision could be in id pattern
+            try:
+                int(fields[1])
+                # It is a revision !
+                fields = (None, fields[0], fields[1])
+            except ValueError:
+                pass
+        if fields[0] is not None and fields[1] is not None and fields[2] is None:
+            # Base version link, should not be allowed because content can be changed externally
+            return None
+        base = "https://jsfiddle.net/{}/embedded/result,js,html,css/"
+        return base.format("/".join([t for t in fields if t is not None]))
 
 
-def makeExtension(configs=None):
-    return VideoExtension(configs=configs)
-
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+def makeExtension(*args, **kwargs):
+    return VideoExtension(*args, **kwargs)
