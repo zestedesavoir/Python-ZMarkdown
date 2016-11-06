117e5ae21a90e88adfe7cf2c2b762cf6d403fbe3
Christophe Gabard <christophe.gabard@gmail.com>
2016-11-06 21:26:33 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 from __future__ import with_statement
-import sys
 import os
 from distutils.core import setup
-from distutils.command.install_scripts import install_scripts
 import imp
 
 
@@ -34,34 +32,6 @@ if version_info[3] == 'alpha' and version_info[4] == 0:
     DEVSTATUS = '2 - Pre-Alpha'
 else:
     DEVSTATUS = dev_status_map[version_info[3]]
-
-# The command line script name.  Currently set to "markdown_py" so as not to
-# conflict with the perl implimentation (which uses "markdown").  We can't use
-# "markdown.py" as the default config on some systems will cause the script to
-# try to import itself rather than the library which will raise an error.
-SCRIPT_NAME = 'markdown_py'
-
-
-class md_install_scripts(install_scripts):
-    """ Customized install_scripts. Create markdown_py.bat for win32. """
-
-    def run(self):
-        install_scripts.run(self)
-
-        if sys.platform == 'win32':
-            try:
-                script_dir = os.path.join(sys.prefix, 'Scripts')
-                script_path = os.path.join(script_dir, SCRIPT_NAME)
-                bat_str = '@"%s" "%s" %%*' % (sys.executable, script_path)
-                bat_path = os.path.join(self.install_dir, '%s.bat' % SCRIPT_NAME)
-                f = open(bat_path, 'w')
-                f.write(bat_str)
-                f.close()
-                print('Created: %s' % bat_path)
-            except Exception:
-                _, err, _ = sys.exc_info()  # for both 2.x & 3.x compatability
-                print('ERROR: Unable to create %s: %s' % (bat_path, err))
-
 
 long_description = '''
 This is a Python implementation of John Gruber's Markdown_.
@@ -96,7 +66,6 @@ setup(name='Markdown',
       maintainer_email='waylan.limberg [at] icloud.com',
       license='BSD License',
       packages=['markdown', 'markdown.extensions'],
-      cmdclass={'install_scripts': md_install_scripts},
       classifiers=['Development Status :: %s' % DEVSTATUS,
                    'License :: OSI Approved :: BSD License',
                    'Operating System :: OS Independent',
