0a2d124edf638d14f75e5bdeccb559cc26cff18c
Christophe Gabard <christophe.gabard@gmail.com>
2016-11-20 15:00:29 +0100

    Cleaning fenced code

--- markdown/extensions/fenced_code.py
+++ markdown/extensions/fenced_code.py
@@ -36,13 +36,7 @@ class FencedBlockProcessor(BlockProcessor):
     def test(self, parent, block):
         return bool(self.re.search(block))
 
-    def run(self, parent, blocks):
-        first_block = blocks[0]
-        m = self.re.search(first_block)
-        if not m:  # pragma: no cover
-            # Run should only be fired if test() return True, then this should never append
-            # Do not raise an exception because exception should never be generated.
-            return False
+    def _parse_argument(self, m):
         gd = m.groupdict()
 
         fence = gd['fence']
@@ -50,7 +44,7 @@ class FencedBlockProcessor(BlockProcessor):
         arglist = gd['arglist']
         interspace = gd['interspace']
         if lang and arglist and len(interspace) == 0:
-            arglist = lang+arglist
+            arglist = lang + arglist
             lang = ""
         # Parse arguments
         linenostart = (1,)
@@ -79,8 +73,10 @@ class FencedBlockProcessor(BlockProcessor):
                 # unknown argument
                 return False
         linenostart, = linenostart
+        return fence, lang, linenostart, hl_lines
 
-        # Search end
+    def _extract_content_position(self, blocks, fence, m):
+        first_block = blocks[0]
         re_end = re.compile(r'(^|\n)' + re.escape(fence) + r'[ ]*(\n|$)')
 
         start_block = (0, m.start(), m.end())
@@ -96,10 +92,17 @@ class FencedBlockProcessor(BlockProcessor):
             if m_end:
                 end_block = (i, m_end.start() + dec, m_end.end() + dec)
                 break
+        return start_block, end_block
+
+    def _extract_content(self, blocks, fence, m):
+        # Search end
+        first_block = blocks[0]
+
+        start_block, end_block = self._extract_content_position(blocks, fence, m)
 
         if end_block[0] < 0:
             # Block not ended, do not transform
-            return False
+            return None, None, None
 
         # Split blocks into before/content aligned/ending
         before = first_block[:start_block[1]]
@@ -119,11 +122,50 @@ class FencedBlockProcessor(BlockProcessor):
             else:
                 end_index = len(block)
 
-            content.append(block[start_index: end_index+1])
+            content.append(block[start_index: end_index + 1])
             if i == end_block[0]:
                 break
 
         content = "\n\n".join(content)
+        return before, content, after
+
+    def extract_code(self, content, lang, hl_lines, linenostart):
+        # If config is not empty, then the codehighlite extension
+        # is enabled, so we call it to highlight the code
+        if self.codehilite_conf:
+            highliter = CodeHilite(content,
+                                   linenums=self.codehilite_conf['linenums'][0],
+                                   guess_lang=self.codehilite_conf['guess_lang'][0],
+                                   css_class=self.codehilite_conf['css_class'][0],
+                                   style=self.codehilite_conf['pygments_style'][0],
+                                   lang=(lang or None),
+                                   noclasses=self.codehilite_conf['noclasses'][0],
+                                   hl_lines=hl_lines,
+                                   linenostart=linenostart)
+
+            code = highliter.hilite()
+        else:
+            if lang:
+                cls_lang = self.LANG_TAG % lang
+            else:
+                cls_lang = ''
+            code = self.CODE_WRAP % (cls_lang, self._escape(content))
+        return code
+
+    def run(self, parent, blocks):
+        first_block = blocks[0]
+        m = self.re.search(first_block)
+        if not m:  # pragma: no cover
+            # Run should only be fired if test() return True, then this should never append
+            # Do not raise an exception because exception should never be generated.
+            return False
+
+        fence, lang, linenostart, hl_lines = self._parse_argument(m)
+
+        before, content, after = self._extract_content(blocks, fence, m)
+
+        if before is None:
+            return False
 
         # Check for code hilite extension
         if not self.checked_for_codehilite:
@@ -137,27 +179,7 @@ class FencedBlockProcessor(BlockProcessor):
         if before:
             self.parser.parseBlocks(parent, [before])
 
-        # If config is not empty, then the codehighlite extension
-        # is enabled, so we call it to highlight the code
-        if self.codehilite_conf:
-            highliter = CodeHilite(content,
-                                   linenums=self.codehilite_conf['linenums'][0],
-                                   guess_lang=self.codehilite_conf['guess_lang'][0],
-                                   css_class=self.codehilite_conf['css_class'][0],
-                                   style=self.codehilite_conf['pygments_style'][0],
-                                   lang=(lang or None),
-                                   noclasses=self.codehilite_conf['noclasses'][0],
-                                   hl_lines=hl_lines,
-                                   linenostart=linenostart
-                                   )
-
-            code = highliter.hilite()
-        else:
-            if lang:
-                cls_lang = self.LANG_TAG % lang
-            else:
-                cls_lang = ''
-            code = self.CODE_WRAP % (cls_lang, self._escape(content))
+        code = self.extract_code(content, lang, hl_lines, linenostart)
 
         div_wrapper = util.etree.SubElement(parent, 'div')
         div_wrapper.text = util.RawHTMLString(code)
