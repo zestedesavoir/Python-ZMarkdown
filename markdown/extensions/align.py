b685c6a857853b30ceae594fa901c694bfabbcd0
Christophe Gabard <cgabard@users.noreply.github.com>
2016-11-20 16:02:16 +0100

    Some cleaning (#123)
    * Cleaning fenced code
    
    * Remove remaining cli elements
    
    * Cleaning codehilite
    
    * Cleaning align
    
    * Cleaning zds extension
    
    * Minor fix
    
    * revert for python 2 compatibility
    
    * Fix flake8

--- markdown/extensions/align.py
+++ markdown/extensions/align.py
@@ -21,21 +21,15 @@ class AlignProcessor(BlockProcessor):
     def test(self, parent, block):
         return bool(self.REStart.search(block))
 
-    def run(self, parent, blocks):
+    def _extract_position(self, blocks, m):
+        first_block = blocks[0]
+        start_block = (0, m.start(), m.end())
 
-        FirstBlock = blocks[0]
-        m = self.REStart.search(FirstBlock)
-        if not m:  # pragma: no cover
-            # Run should only be fired if test() return True, then this should never append
-            # Do not raise an exception because exception should never be generated.
-            return False
-        StartBlock = (0, m.start(), m.end())
-
-        EndBlock = (-1, -1, -1)
+        end_block = (-1, -1, -1)
         content_align = "left"
         for i in range(len(blocks)):
             if i == 0:
-                txt = FirstBlock[m.end() + 1:]
+                txt = first_block[m.end() + 1:]
                 dec = m.end() - m.start() + 1
             else:
                 txt = blocks[i]
@@ -47,47 +41,65 @@ class AlignProcessor(BlockProcessor):
             if len(t_ends) > 0:
                 # retrieve first matching re
                 selected_align, mEnd = min(t_ends, key=lambda e: e[1].start())
-                EndBlock = (i, mEnd.start() + dec, mEnd.end() + dec)
+                end_block = (i, mEnd.start() + dec, mEnd.end() + dec)
                 content_align = self._kind_align[selected_align]
                 break
 
-        if EndBlock[0] < 0:
+        if end_block[0] < 0:
             # Block not ended, do not transform
-            return False
+            return None, None, None
+        return start_block, content_align, end_block
+
+    def _extract_content(self, blocks, m):
+        first_block = blocks[0]
+        start_block, content_align, end_block = self._extract_position(blocks, m)
+        if start_block is None:
+            return None, None, None, None
 
         # Split blocks into before/content aligned/ending
         # There should never have before and ending because regex require that the expression is starting/ending the
         # block. This is set for security : if regex are updated the code should always work.
-        Before = FirstBlock[:StartBlock[1]]
-        Content = []
-        After = blocks[EndBlock[0]][EndBlock[2]:]
-        for i in range(0, EndBlock[0] + 1):
+        before = first_block[:start_block[1]]
+        content = []
+        after = blocks[end_block[0]][end_block[2]:]
+        for i in range(0, end_block[0] + 1):
             blck = blocks.pop(0)
 
-            if i == StartBlock[0]:
-                startIndex = StartBlock[2]
+            if i == start_block[0]:
+                startIndex = start_block[2]
             else:
                 startIndex = 0
 
-            if i == EndBlock[0]:
-                endIndex = EndBlock[1]
+            if i == end_block[0]:
+                endIndex = end_block[1]
             else:
                 endIndex = len(blck)
 
-            Content.append(blck[startIndex: endIndex])
+            content.append(blck[startIndex: endIndex])
 
-        Content = "\n\n".join(Content)
+        content = "\n\n".join(content)
+        return before, content, after, content_align
 
-        if Before:  # pragma: no cover
+    def run(self, parent, blocks):
+
+        first_block = blocks[0]
+        m = self.REStart.search(first_block)
+        if not m:  # pragma: no cover
+            # Run should only be fired if test() return True, then this should never append
+            # Do not raise an exception because exception should never be generated.
+            return False
+
+        before, content, after, content_align = self._extract_content(blocks, m)
+        if before is None:
+            return False
+
+        if before:  # pragma: no cover
             # This should never occur because regex require that the expression is starting the block.
             # Do not raise an exception because exception should never be generated.
-            self.parser.parseBlocks(parent, [Before])
+            self.parser.parseBlocks(parent, [before])
 
         sibling = self.lastChild(parent)
-        if (sibling and
-                sibling.tag == "div" and
-                "align" in sibling.attrib and
-                sibling.attrib["align"] == content_align):
+        if sibling and sibling.tag == "div" and sibling.attrib.get("align", None) == content_align:
             # If previous block is the same align content, merge it !
             h = sibling
             if h.text:  # pragma: no cover
@@ -98,12 +110,12 @@ class AlignProcessor(BlockProcessor):
             h = util.etree.SubElement(parent, 'div')
             h.set("align", content_align)
 
-        self.parser.parseChunk(h, Content)
+        self.parser.parseChunk(h, content)
 
-        if After:  # pragma: no cover
+        if after:  # pragma: no cover
             # This should never occur because regex require that the expression is ending the block.
             # Do not raise an exception because exception should never be generated.
-            blocks.insert(0, After)
+            blocks.insert(0, after)
 
 
 class AlignExtension(markdown.extensions.Extension):
